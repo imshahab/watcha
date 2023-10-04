e920c11904dbae6ad7d638620b208b37bfbac5a8
Shahab Hosseini <63445114+theshahabhosseini@users.noreply.github.com>
2023-10-04 11:47:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     async function getDetails(id) {
-        const response = await fetch(`http://www.omdbapi.com/?apiKey=${apiKey}&i=${id}`)
+        const response = await fetch(`https://www.omdbapi.com/?apiKey=${apiKey}&i=${id}`)
         const movieObj = await response.json()
         return movieObj
     }
